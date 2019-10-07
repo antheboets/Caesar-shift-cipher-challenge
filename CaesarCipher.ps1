4714820639771eed01a746ea2e60ec261405135a
Anthe Boets <44194665+antheboets@users.noreply.github.com>
2019-10-07 14:18:21 +0200

    Adding Caeser shift cipher cripte

---
+++ CaesarCipher.ps1
@@ -0,0 +1,48 @@
+$data = Get-Content ((Split-Path $script:MyInvocation.MyCommand.Path)+ "\" + "input.txt")
+$output = ""
+$shiftCount = 26
+$output += "=================ORIGIN================="
+$output += "`n"
+$output += "`n"
+for($i  = 0; $i -le $shiftCount; $i++)
+{
+    foreach($line in $data)
+    {
+        foreach($letter in $line.ToCharArray())
+        {
+            if($letter -match "[a-zA-Z]")
+            {
+                if(([int]$letter -ge 65 -and [int]$letter -le 90 -and ([int]$letter + $i) -ge 91 ) -or ([int]$letter -ge 97 -and [int]$letter -le 122 -and ([int]$letter + $i) -ge -123))
+                {
+                    if($letter -match "[a-z]")
+                    {
+                        $overFlow = ($i + [int]$letter - 122)
+                        $output += [char](97 + $overFlow -1)
+                    }
+                    else
+                    {
+                        $overFlow = ($i + [int]$letter - 90)
+                        $output += [char](65 + $overFlow -1)
+                    }
+                }
+                else
+                {
+                    $output += [char]($i + [int]$letter)
+                }
+            }
+            else
+            {
+                $output += $letter
+            }
+        }
+        $output += "`n"
+    }
+    if($i -lt $shiftCount)
+    {
+        $output += "`n"
+        $output += "=================SHIFT "  + ($i + 1) + "================="
+        $output += "`n"
+        $output += "`n"
+    }
+}
+Set-Content -Path ((Split-Path $script:MyInvocation.MyCommand.Path)+ "\" + "output.txt") -Value $output
